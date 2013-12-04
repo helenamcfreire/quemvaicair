53eceb698d99c35486f1ab74f80462500fdaa1b8
Helena Freire <helenamcfreire@gmail.com>
2013-12-04 12:34:20 -0200

    initial commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,32 +6,36 @@ class HomeController < ApplicationController
   end
 
   def create
+
     @order = Order.new(params[:order])
 
     if @order.save
       flash[:success] = I18n.t :success
+
+      payment = PagSeguro::PaymentRequest.new
+      payment.reference = @order.id
+      payment.redirect_url = 'http://quemvaicair.herokuapp.com'
+
+      payment.items << {
+          id: @order.product_id,
+          description: t(:bolao),
+          amount: 1.00,
+          weight: 0
+      }
+
+      response = payment.register
+
+      if response.errors.any?
+        raise response.errors.join("\n")
+      else
+        redirect_to response.url
+      end
+
     else
       flash[:error] = I18n.t :error
     end
 
-    payment = PagSeguro::PaymentRequest.new
-    payment.reference = @order.id
-    payment.redirect_url = 'http://quemvaicair.herokuapp.com'
-
-    payment.items << {
-        id: @order.product_id,
-        description: t(:bolao),
-        amount: 1.00,
-        weight: 0
-    }
-
-    response = payment.register
-
-    if response.errors.any?
-      raise response.errors.join("\n")
-    else
-      redirect_to response.url
-    end
+    redirect_to root_path
 
   end
 
